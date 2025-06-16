6df5b71f3dca9ad427d96916696d2a4f17d80c5d
Zyad <zyadhammad531@gmail.com>
2025-06-16 23:54:21 +0300

    feat: add category operations

--- app/src/main/java/com/moscow/tudee/data/service/TasksServicesImpl.kt
+++ app/src/main/java/com/moscow/tudee/data/service/TasksServicesImpl.kt
@@ -1,7 +1,24 @@
 package com.moscow.tudee.data.service
 
+import com.moscow.tudee.domain.entity.Category
 import com.moscow.tudee.domain.service.TasksServices
+import kotlinx.coroutines.flow.Flow
 
 class TasksServicesImpl: TasksServices {
+    override suspend fun getCategories(): Flow<List<Category>> {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun addCategory(category: Category) {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun updateCategory(category: Category) {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun deleteCategory(categoryId: String) {
+        TODO("Not yet implemented")
+    }
 
 }
--- app/src/main/java/com/moscow/tudee/domain/service/TasksServices.kt
+++ app/src/main/java/com/moscow/tudee/domain/service/TasksServices.kt
@@ -1,4 +1,15 @@
 package com.moscow.tudee.domain.service
 
+import com.moscow.tudee.domain.entity.Category
+import kotlinx.coroutines.flow.Flow
+
 interface TasksServices {
+    suspend fun getCategories(): Flow<List<Category>>
+
+    suspend fun addCategory(category: Category)
+
+    suspend fun updateCategory(category: Category)
+
+    suspend fun deleteCategory(categoryId: String)
+
 }
