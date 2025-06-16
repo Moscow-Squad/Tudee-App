d077ce839fad6d77b0c68457c0093a606e565147
Menna Srour <111497966+MennaSrour@users.noreply.github.com>
2025-06-16 06:10:24 +0300

    feat: add Days component with improved layout and modifier extensions
    - Add Days UI component
    - Improve Column arrangement and text styling
    - Introduce Modifier.applyIf extension
    - Make onDayClick parameter required for better click handling
    - Refactore ModifierExtensions to a dedicated package

---
+++ app/src/main/java/com/moscow/tudee/presentation/designSystem/component/modifier/ModifierExtensions.kt
@@ -0,0 +1,13 @@
+package com.moscow.tudee.presentation.designSystem.component.modifier
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun Modifier.applyIf(condition: Boolean, modifier: @Composable Modifier.() -> Modifier): Modifier {
+    return if (condition) {
+        then(modifier())
+    } else {
+        this
+    }
+}
