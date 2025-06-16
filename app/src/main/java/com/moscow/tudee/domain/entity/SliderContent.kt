a2c3f5797b9ff731eb7b9587cf6b63db3d1f4deb
mada <mohamedibra3232@gmail.com>
2025-06-16 13:21:07 +0300

    feat: add slider content sealed class with static objects for different slider states and companion object factory method

---
+++ app/src/main/java/com/moscow/tudee/domain/entity/SliderContent.kt
@@ -0,0 +1,49 @@
+package com.moscow.tudee.domain.entity
+
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+import com.moscow.tudee.R
+
+sealed class SliderContent(
+    @StringRes val title: Int,
+    @DrawableRes val emojiState: Int,
+    @StringRes val description: Int,
+    @DrawableRes val imageResId: Int,
+) {
+    object StayWorking : SliderContent(
+        title = R.string.stay_working,
+        emojiState = R.drawable.ic_status_neutral,
+        description = R.string.stay_working_description,
+        imageResId = R.drawable.im_robot_neutral
+    )
+
+    object Tadaa : SliderContent(
+        title = R.string.tadaa,
+        emojiState = R.drawable.ic_status_happy,
+        description = R.string.tadaa_description,
+        imageResId = R.drawable.im_robot_happy
+    )
+
+    object ZeroProgress : SliderContent(
+        title = R.string.zero_progress,
+        emojiState = R.drawable.ic_status_angry,
+        description = R.string.zero_progress_description,
+        imageResId = R.drawable.im_robot_angry
+    )
+
+    object NothingOnYourList : SliderContent(
+        title = R.string.Noting_to_do,
+        emojiState = R.drawable.ic_status_sad,
+        description = R.string.Noting_to_do_description,
+        imageResId = R.drawable.im_robot_neutral
+    )
+
+    companion object {
+        fun from(state: SliderState): SliderContent = when(state) {
+            SliderState.STAY_WORKING -> StayWorking
+            SliderState.TADAA -> Tadaa
+            SliderState.ZERO_PROGRESS -> ZeroProgress
+            SliderState.NOTHING_ON_YOUR_LIST -> NothingOnYourList
+        }
+    }
+}
