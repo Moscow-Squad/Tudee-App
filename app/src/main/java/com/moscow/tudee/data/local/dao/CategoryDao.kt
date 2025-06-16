6f39276336572fec3d604393178bdfc791fbbf88
alaafawzy <alaaf9443@gmail.com>
2025-06-17 00:25:21 +0300

    feat :add category dao operation and change abstract class to interface

--- app/src/main/java/com/moscow/tudee/data/local/dao/CategoryDao.kt
+++ app/src/main/java/com/moscow/tudee/data/local/dao/CategoryDao.kt
@@ -1,8 +1,23 @@
 package com.moscow.tudee.data.local.dao
 
 import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
+import com.moscow.tudee.domain.entity.Category
+
 
 @Dao
-abstract class CategoryDao {
+interface CategoryDao {
+    @Query("SELECT * FROM categories_table")
+    suspend fun getCategories(): List<Category>?
 
+    @Insert
+    suspend fun addCategory(category: Category)
+
+    @Update
+    suspend fun updateCategory(category: Category)
+
+    @Query("DELETE FROM categories_table WHERE id = :categoryId")
+    suspend fun deleteCategory(categoryId: String)
 }
