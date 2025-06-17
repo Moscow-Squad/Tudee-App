56ecc0013f63174e33f073563fd0a618f0bce1b7
Zyad Nasser <zyadhammad531@gmail.com>
2025-06-17 22:06:35 +0300

    feature/#22-categories-crud-operations-in-domain-and-data (#44)
    * feat: add category operations
    
    * feat :add category dao operation and change abstract class to interface
    
    * feat :add di for category dao and task dao
    
    * feat :change return type for get category fun
    
    * feat :add implementation for task service impl
    
    * feat: add tasks services to di and rename data module
    
    * fix: change tasks service function types to room entity instead of domain entity and add mapper from domain to room entity
    
    * fix: github reviews
    - remove nullability from the get categories functions return type
    - change the domain category to room category mapper function name to be more specific
    
    ---------
    
    Co-authored-by: alaafawzy <alaaf9443@gmail.com>

--- app/src/main/java/com/moscow/tudee/data/service/TasksServicesImpl.kt
+++ app/src/main/java/com/moscow/tudee/data/service/TasksServicesImpl.kt
@@ -1,7 +1,26 @@
 package com.moscow.tudee.data.service
 
+import com.moscow.tudee.data.local.dao.CategoryDao
+import com.moscow.tudee.data.local.mapper.getRoomEntityFromCategory
+import com.moscow.tudee.data.local.mapper.toCategory
+import com.moscow.tudee.domain.entity.Category
 import com.moscow.tudee.domain.service.TasksServices
 
-class TasksServicesImpl: TasksServices {
+class TasksServicesImpl(private val categoryDao: CategoryDao) : TasksServices {
+    override suspend fun getCategories(): List<Category> {
+        return categoryDao.getCategories().map { it.toCategory() }
+    }
+
+    override suspend fun addCategory(category: Category) {
+        categoryDao.addCategory(getRoomEntityFromCategory(category))
+    }
+
+    override suspend fun updateCategory(category: Category) {
+        categoryDao.updateCategory(getRoomEntityFromCategory(category))
+    }
+
+    override suspend fun deleteCategory(categoryId: Long) {
+        categoryDao.deleteCategory(categoryId)
+    }
 
 }
--- app/src/main/java/com/moscow/tudee/domain/service/TasksServices.kt
+++ app/src/main/java/com/moscow/tudee/domain/service/TasksServices.kt
@@ -1,4 +1,14 @@
 package com.moscow.tudee.domain.service
 
+import com.moscow.tudee.domain.entity.Category
+
 interface TasksServices {
+    suspend fun getCategories(): List<Category>
+
+    suspend fun addCategory(category: Category)
+
+    suspend fun updateCategory(category: Category)
+
+    suspend fun deleteCategory(categoryId: Long)
+
 }
