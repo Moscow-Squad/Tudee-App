b3daf4b4f598a33846a2bd906fdaa0ecaf08d01d
mada <mohamedibra3232@gmail.com>
2025-06-17 18:40:52 +0300

    feat: setup Koin modules for splash service and view model

--- app/src/main/java/com/moscow/tudee/di/data/databaseModule.kt
+++ app/src/main/java/com/moscow/tudee/di/data/databaseModule.kt
@@ -2,6 +2,9 @@ package com.moscow.tudee.di.data
 
 import androidx.room.Room
 import com.moscow.tudee.data.local.database.TudeeDatabase
+import com.moscow.tudee.data.service.SplashServiceImpl
+import com.moscow.tudee.domain.service.SplashService
+import org.koin.android.ext.koin.androidApplication
 import org.koin.android.ext.koin.androidContext
 import org.koin.dsl.module
 
@@ -11,4 +14,5 @@ val databaseModule = module {
     single<TudeeDatabase> {
         Room.databaseBuilder(androidContext(), TudeeDatabase::class.java, TUDEE_DATABASE).build()
     }
+    single<SplashService> { SplashServiceImpl(androidApplication()) }
 }
