d7f779d9255fcaceb168a3c229657cef690220e6
mada <mohamedibra3232@gmail.com>
2025-06-18 21:38:29 +0300

    refactor: use constructor dsl

--- app/src/main/java/com/moscow/tudee/di/presentationModule.kt
+++ app/src/main/java/com/moscow/tudee/di/presentationModule.kt
@@ -1,9 +1,9 @@
 package com.moscow.tudee.di
 
 import com.moscow.tudee.presentation.viewmodel.SplashViewModel
-import org.koin.core.module.dsl.viewModel
+import org.koin.core.module.dsl.viewModelOf
 import org.koin.dsl.module
 
 val presentationModule = module {
-    viewModel { SplashViewModel(get()) }
+    viewModelOf(::SplashViewModel)
 }
