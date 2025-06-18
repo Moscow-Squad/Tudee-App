29114fa4f67e2ec2ef3e494d2904a04096610af4
AdelTamer <adeltammer@gmail.com>
2025-06-18 19:58:18 +0300

    feat: create category bar

---
+++ app/src/main/java/com/moscow/tudee/presentation/category/CategoryBar.kt
@@ -0,0 +1,82 @@
+package com.moscow.tudee.presentation.category
+
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.moscow.tudee.R
+import com.moscow.tudee.presentation.designSystem.theme.Theme.colors
+import com.moscow.tudee.presentation.designSystem.theme.Theme.textStyle
+
+@Composable
+fun CategoryBar(
+    categoryLabel: String,
+    modifier: Modifier = Modifier,
+) {
+    Row(
+        modifier = modifier
+            .fillMaxWidth()
+            .background(colors.surfaceHigh),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Box(
+            modifier = Modifier
+                .padding(end = 12.dp)
+                .size(40.dp)
+                .clip(CircleShape)
+                .border(BorderStroke(1.dp, colors.stroke), CircleShape),
+            contentAlignment = Alignment.Center
+        ) {
+            Icon(
+                painter = painterResource(R.drawable.ic_arrow_head_back),
+                contentDescription = "Back Arrow",
+                tint = colors.body
+            )
+        }
+
+        Text(
+            text = categoryLabel,
+            style = textStyle.title.large,
+            color = colors.title
+        )
+
+        Spacer(modifier = Modifier.weight(1f))
+
+        Box(
+            modifier = Modifier
+                .size(40.dp)
+                .clip(CircleShape)
+                .border(BorderStroke(1.dp, colors.stroke), CircleShape),
+            contentAlignment = Alignment.Center
+        ) {
+            Icon(
+                painter = painterResource(R.drawable.ic_pencil_edit),
+                contentDescription = "Pencil Edit",
+                tint = colors.body
+            )
+        }
+    }
+}
+
+@Preview(apiLevel = 34)
+@Composable
+fun CategoryBar_Preview() {
+    CategoryBar(
+        categoryLabel = "Reading Novel"
+    )
+}
