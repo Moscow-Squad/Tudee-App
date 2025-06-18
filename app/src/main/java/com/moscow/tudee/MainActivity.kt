ba6d276d29d98e780690c9d98754a2acc2f3a9a8
pola george <86931053+poula22@users.noreply.github.com>
2025-06-18 22:20:14 +0300

    chore: remove unused code

--- app/src/main/java/com/moscow/tudee/MainActivity.kt
+++ app/src/main/java/com/moscow/tudee/MainActivity.kt
@@ -4,17 +4,10 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.appcompat.app.AppCompatDelegate
-import androidx.compose.foundation.background
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.systemBarsPadding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -24,10 +17,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import com.moscow.tudee.presentation.designSystem.component.scaffold.TudeeScaffold
 import com.moscow.tudee.presentation.designSystem.component.tudeeSwitch.TudeeSwitch
 import com.moscow.tudee.presentation.designSystem.theme.Theme
 import com.moscow.tudee.presentation.designSystem.theme.ThemeState
@@ -49,61 +39,8 @@ class MainActivity : ComponentActivity() {
                 }
             }
 
-            var isDay by remember {
-                mutableStateOf(false)
-            }
-
             TudeeTheme(state = themeState) {
-                val localState = Theme.state
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Box(Modifier.padding(innerPadding)) {
-                        TudeeSwitch(
-                            isLightTheme = localState.isDark,
-                            onToggleState = {
-                                localState.onThemeChanged(!localState.isDark)
-                            }
-                        )
-                    }
 
-                }
-
-//                TudeeScaffold(
-//                    content = {
-//                        Column(
-//                            Modifier
-//                                .fillMaxSize()
-//                                .background(Color.Red)
-//                        ) { TudeeSwitch(isLightTheme = true) { }}
-//                    },
-//                    bottomBar = {
-//                        Column(
-//                            Modifier
-//                                .fillMaxWidth()
-//                                .height(100.dp)
-//                                .background(Color.Green)
-//                        ) {
-//                            TudeeSwitch(isLightTheme = true) { }
-//                        }
-//                    },
-//                    topBar = {
-//                        Column(
-//                            Modifier
-//                                .fillMaxWidth()
-//                                .background(Color.Blue)
-//                        ) {
-//                            TudeeSwitch(isLightTheme = true) { }
-//                        }
-//                    },
-//                    floatingActionButton = {
-//                        Column(
-//                            Modifier
-//                                .size(50.dp)
-//                                .background(Color.Yellow)
-//                        ) {
-//                            TudeeSwitch(isLightTheme = true) { }
-//                        }
-//                    }
-//                )
             }
         }
     }
