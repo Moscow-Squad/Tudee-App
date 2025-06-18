d9320890169fd92d54b532df90ec517480a56f1d
Israa Mohamed <e.mohamed@shabek.com>
2025-06-18 14:16:37 +0300

    feat: refactor bottom navigation bar

---
+++ app/src/main/java/com/moscow/tudee/presentation/model/BottomNavigationDestination.kt
@@ -0,0 +1,32 @@
+package com.moscow.tudee.presentation.model
+
+import androidx.annotation.DrawableRes
+import com.moscow.tudee.R
+import com.moscow.tudee.navigation.entry.CategoriesScreen
+import com.moscow.tudee.navigation.entry.HomeScreen
+import com.moscow.tudee.navigation.entry.TasksScreen
+
+enum class BottomNavigationDestination(
+    val route: Any,
+    @DrawableRes val notSelectedIcon: Int,
+    @DrawableRes val selectedIcon: Int,
+    ){
+    Home(
+        route = HomeScreen,
+        notSelectedIcon = R.drawable.ic_home_outlined,
+        selectedIcon = R.drawable.ic_home_filled ,
+    ),
+
+    Tasks(
+        route = TasksScreen,
+        notSelectedIcon = R.drawable.ic_document_outlined,
+        selectedIcon = R.drawable.ic_document_filled,
+    ),
+
+    Categories(
+        route = CategoriesScreen,
+        notSelectedIcon = R.drawable.ic_menu_circle_outlined,
+        selectedIcon = R.drawable.ic_menu_circle_filled ,
+    )
+
+}
