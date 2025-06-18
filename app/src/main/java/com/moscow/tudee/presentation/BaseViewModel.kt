c04344fd6e719ee42a3588ae065ca559336d35c5
Fares <dev.faresmohamed@gmail.com>
2025-06-18 13:47:21 +0300

    feat: implement base view model

---
+++ app/src/main/java/com/moscow/tudee/presentation/BaseViewModel.kt
@@ -0,0 +1,35 @@
+package com.moscow.tudee.presentation
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+
+abstract class BaseViewModel<T>(
+    initialState: T
+) : ViewModel() {
+
+    private val _uiState = MutableStateFlow(initialState)
+    val uiState: StateFlow<T> = _uiState
+
+    protected fun updateState(transform: (T) -> T) {
+        _uiState.value = transform(_uiState.value)
+    }
+
+    protected fun <R> launchWithResult(
+        action: suspend () -> R,
+        onSuccess: (R) -> Unit,
+        onError: (Throwable) -> Unit,
+        onStart: () -> Unit = {},
+        onFinally: () -> Unit = {}
+    ) {
+        viewModelScope.launch {
+            onStart()
+            runCatching { action() }
+                .onSuccess(onSuccess)
+                .onFailure(onError)
+            onFinally()
+        }
+    }
+}
