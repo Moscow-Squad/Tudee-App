3503eb96fc69738c25c75dab27b78dc71340d7a3
Israa Mohamed <e.mohamed@shabek.com>
2025-06-18 14:23:12 +0300

    feat: add route files for each screen

---
+++ app/src/main/java/com/moscow/tudee/presentation/ui/home/HomeRoute.kt
@@ -0,0 +1,29 @@
+package com.moscow.tudee.presentation.ui.home
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.navigation.NavGraphBuilder
+import com.moscow.tudee.navigation.entry.HomeScreen
+import com.moscow.tudee.navigation.extensions.tudeeComposable
+
+fun NavGraphBuilder.homeRoute() {
+
+    tudeeComposable<HomeScreen> {
+        Column(
+            modifier = Modifier
+                .fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Text(
+                text = "Home screen",
+                fontWeight = FontWeight.Bold
+            )
+        }
+    }
+}
---
+++ app/src/main/java/com/moscow/tudee/presentation/ui/main/MainScreen.kt
@@ -0,0 +1,35 @@
+package com.moscow.tudee.presentation.ui.main
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+import com.moscow.tudee.navigation.MainNavGraph
+import com.moscow.tudee.presentation.designSystem.component.BottomNavBar
+import com.moscow.tudee.presentation.designSystem.theme.Theme
+import com.moscow.tudee.presentation.model.BottomNavigationDestination
+
+@Composable
+fun MainScreen(
+    navController: NavHostController = rememberNavController(),
+){
+
+    Scaffold(
+        modifier = Modifier.fillMaxSize(),
+        containerColor = Theme.colors.surface,
+        bottomBar = {
+            BottomNavBar(
+                bottomNavigationItems = BottomNavigationDestination.entries,
+                navController = navController
+            )
+        },
+    ) { innerPadding ->
+        MainNavGraph(
+            navController = navController,
+            paddingValues = innerPadding,
+        )
+    }
+}
+
