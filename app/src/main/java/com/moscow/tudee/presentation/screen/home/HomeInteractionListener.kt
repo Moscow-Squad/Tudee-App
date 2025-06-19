0ccfff2b3f67c696de4d85e39bba837a2229cdd3
alaafawzy <alaaf9443@gmail.com>
2025-06-19 22:47:19 +0300

    feat: add home action function  to handle all home actions

--- app/src/main/java/com/moscow/tudee/presentation/screen/home/HomeInteractionListener.kt
+++ app/src/main/java/com/moscow/tudee/presentation/screen/home/HomeInteractionListener.kt
@@ -1,6 +1,7 @@
 package com.moscow.tudee.presentation.screen.home
 
 import com.moscow.tudee.domain.entity.Task
+import kotlinx.datetime.LocalDateTime
 
 interface HomeInteractionListener {
 
@@ -11,4 +12,26 @@ interface HomeInteractionListener {
     fun onAddTask(taskDetails: HomeState.TaskDetails)
 
     fun onViewAllClick(taskStatus: Task.Status)
+
+    fun onEditTaskClick(taskDetails: HomeState.TaskDetails)
+
+    fun onMoveToDoneClick(taskDetails: HomeState.TaskDetails)
+
+    fun onSaveTaskClick(taskDetails: HomeState.TaskDetails)
+
+    fun onPriorityClick(taskPriority: Task.Priority)
+
+    fun onTitleChange(newTitle: String)
+
+    fun onDescriptionChange(newDescription: String)
+
+    fun onDateChange(newDate: LocalDateTime)
+
+    fun onDismissEditBottomSheet()
+
+    fun onDismissAddBottomSheet()
+
+    fun onDismissDetailsBottomSheet()
+
+
 }
