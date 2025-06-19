5140ff16c59b256fff2ea3682b183aacac13d6c7
Ahmad Kharfan <plamoplamo12@gmail.com>
2025-06-20 01:45:18 +0300

    feat: add all predefined categories

--- app/src/main/java/com/moscow/tudee/di/data/dataModule.kt
+++ app/src/main/java/com/moscow/tudee/di/data/dataModule.kt
@@ -5,11 +5,11 @@ import com.moscow.tudee.data.local.DatabaseCallback
 import com.moscow.tudee.data.local.dao.CategoryDao
 import com.moscow.tudee.data.local.dao.TaskDao
 import com.moscow.tudee.data.local.database.TudeeDatabase
-import com.moscow.tudee.data.service.SplashServiceImpl
 import com.moscow.tudee.data.service.CategoryServicesImpl
+import com.moscow.tudee.data.service.SplashServiceImpl
 import com.moscow.tudee.data.service.TasksServicesImpl
-import com.moscow.tudee.domain.service.SplashService
 import com.moscow.tudee.domain.service.CategoryServices
+import com.moscow.tudee.domain.service.SplashService
 import com.moscow.tudee.domain.service.TasksServices
 import org.koin.android.ext.koin.androidApplication
 import org.koin.android.ext.koin.androidContext
@@ -18,7 +18,29 @@ import org.koin.dsl.module
 private const val TUDEE_DATABASE = "tudee_database"
 
 val dataModule = module {
-    single { listOf("quran", "shopping", "education") }
+
+    single {
+        listOf(
+            "quran",
+            "shopping",
+            "education",
+            "medical",
+            "gym",
+            "entertainment",
+            "cooking",
+            "family & friend",
+            "traveling",
+            "agriculture",
+            "coding",
+            "adoration",
+            "fixing bugs",
+            "cleaning",
+            "work",
+            "budgeting",
+            "self-care",
+            "event"
+        )
+    }
 
     single<TudeeDatabase> {
         Room.databaseBuilder(
