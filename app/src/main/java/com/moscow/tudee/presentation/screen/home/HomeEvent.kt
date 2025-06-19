0decd5b8e795eb515c85b4e344d18789b9e444b2
Zyad <zyadhammad531@gmail.com>
2025-06-20 01:47:10 +0300

    refactor: remove redundant events

--- app/src/main/java/com/moscow/tudee/presentation/screen/home/HomeEvent.kt
+++ app/src/main/java/com/moscow/tudee/presentation/screen/home/HomeEvent.kt
@@ -3,7 +3,5 @@ package com.moscow.tudee.presentation.screen.home
 import com.moscow.tudee.domain.entity.Task
 
 sealed interface HomeEvent {
-    data object ShowEditTaskBottomSheet : HomeEvent
-    data object OnDoneClick : HomeEvent
     data class ViewAll(val taskStatus: Task.Status) : HomeEvent
 }
