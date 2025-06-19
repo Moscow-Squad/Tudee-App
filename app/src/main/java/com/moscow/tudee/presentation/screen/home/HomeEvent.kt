922197a408f5c7a8f1bcda308d834c5d6faaed81
alaafawzy <alaaf9443@gmail.com>
2025-06-20 01:08:00 +0300

    feat:remove unused events

--- app/src/main/java/com/moscow/tudee/presentation/screen/home/HomeEvent.kt
+++ app/src/main/java/com/moscow/tudee/presentation/screen/home/HomeEvent.kt
@@ -3,12 +3,7 @@ package com.moscow.tudee.presentation.screen.home
 import com.moscow.tudee.domain.entity.Task
 
 sealed interface HomeEvent {
-    data object ShowTaskDetailsBottomSheet : HomeEvent
     data object ShowEditTaskBottomSheet : HomeEvent
-    data object ShowAddTaskBottomSheet : HomeEvent
-    data object DismissEditBottomSheet : HomeEvent
-    data object DismissAddBottomSheet : HomeEvent
-    data object DismissDetailsBottomSheet : HomeEvent
-   data object OnDoneClick : HomeEvent
+    data object OnDoneClick : HomeEvent
     data class ViewAll(val taskStatus: Task.Status) : HomeEvent
 }
