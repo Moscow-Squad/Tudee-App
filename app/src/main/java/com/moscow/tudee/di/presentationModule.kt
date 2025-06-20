1362baa8f77b28056f915640a07bc14a6c945e19
Fares <dev.faresmohamed@gmail.com>
2025-06-20 11:18:56 +0300

    fix: koin dependency for view model

--- app/src/main/java/com/moscow/tudee/di/presentationModule.kt
+++ app/src/main/java/com/moscow/tudee/di/presentationModule.kt
@@ -1,9 +1,13 @@
 package com.moscow.tudee.di
 
+import com.moscow.tudee.presentation.category.categoryScreen.CategoryViewModel
+import com.moscow.tudee.presentation.category.categoryTasks.CategoryTasksViewModel
 import com.moscow.tudee.presentation.ui.splash.viewmodel.SplashViewModel
 import org.koin.core.module.dsl.viewModelOf
 import org.koin.dsl.module
 
 val presentationModule = module {
     viewModelOf(::SplashViewModel)
+    viewModelOf(::CategoryViewModel)
+    viewModelOf(::CategoryTasksViewModel)
 }
