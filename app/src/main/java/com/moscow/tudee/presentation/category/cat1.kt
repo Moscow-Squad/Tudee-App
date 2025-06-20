91a5e28022343bc7b20245f83aebfdd37e8e724f
Fares <dev.faresmohamed@gmail.com>
2025-06-20 10:44:19 +0300

    refactor: remove un used files from category

--- app/src/main/java/com/moscow/tudee/presentation/category/cat1.kt
+++ app/src/main/java/com/moscow/tudee/presentation/category/cat1.kt
@@ -3,68 +3,92 @@ package com.moscow.tudee.presentation.category
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
-import com.moscow.tudee.R
-import com.moscow.tudee.presentation.category.tasks.TasksUiState
-import com.moscow.tudee.presentation.designSystem.component.PriorityChip
 import com.moscow.tudee.presentation.designSystem.component.TaskCard
-import com.moscow.tudee.presentation.designSystem.theme.Theme.colors
 
+//
+//import androidx.compose.foundation.layout.Arrangement
+//import androidx.compose.foundation.layout.PaddingValues
+//import androidx.compose.foundation.lazy.LazyColumn
+//import androidx.compose.foundation.lazy.items
+//import androidx.compose.runtime.Composable
+//import androidx.compose.ui.res.painterResource
+//import androidx.compose.ui.unit.dp
+//import com.moscow.tudee.R
+//import com.moscow.tudee.presentation.category.tasks.TasksUiState
+//import com.moscow.tudee.presentation.designSystem.component.PriorityChip
+//import com.moscow.tudee.presentation.designSystem.component.TaskCard
+//import com.moscow.tudee.presentation.designSystem.theme.Theme.colors
 
-@Composable
-fun CategoryScreen(
-    tasks: List<TasksUiState.TaskUi>
-) {
-    // there is topBar has the Category name  of these tasks "Coding, "Education", ... ??
-    // Where are Tabs (In progress, To Do, Done)
-    LazyColumn(
-        contentPadding = PaddingValues(horizontal = 16.dp, vertical = 12.dp),
-        verticalArrangement = Arrangement.spacedBy(8.dp)
-    ) {
-        items(tasks) { currentTask ->
-            TaskCard(
-                icon = painterResource(currentTask.icon),
-                title = currentTask.title,
-                description = currentTask.description,
-                date = currentTask.date.formattedDate,
-            ) {
-                Priority(priority = currentTask.priority)
-            }
-        }
-    }
-}
+////
+//@Composable
+//fun CategoryScreen(
+//    tasks: List<TasksUiState.TaskUi>
+//) {
+//    // there is topBar has the Category name  of these tasks "Coding, "Education", ... ??
+//    // Where are Tabs (In progress, To Do, Done)
+//    LazyColumn(
+//        contentPadding = PaddingValues(horizontal = 16.dp, vertical = 12.dp),
+//        verticalArrangement = Arrangement.spacedBy(8.dp)
+//    ) {
+//        items(tasks) { currentTask ->
+//            TaskCard(
+//                icon = painterResource(currentTask.icon),
+//                title = currentTask.title,
+//                description = currentTask.description,
+//                date = currentTask.date.formattedDate,
+//            ) {
+//                Priority(priority = currentTask.priority)
+//            }
+//        }
+//    }
+//}
 
-@Composable
-fun Priority(priority: String) {
-    when (priority) {
-        "High" -> {
-            PriorityChip(
-                text = priority,
-                backgroundColor = colors.pinkAccent,
-                icon = painterResource(id = R.drawable.ic_flag)
-            )
-        }
+//@Composable
+//fun CategoryTaskssss(
+//    categoryId:Long,
+//    modifier: Modifier = Modifier) {
+//
+//    Text(
+//        text = "Category ID: $categoryId",
+//        modifier = modifier,
+//        style = com.moscow.tudee.presentation.designSystem.theme.Theme.textStyle.title.large,
+//        color = com.moscow.tudee.presentation.designSystem.theme.Theme.colors.title
+//    )
+//}
 
-        "Medium" -> {
-            PriorityChip(
-                text = priority,
-                backgroundColor = colors.yellowAccent,
-                icon = painterResource(id = R.drawable.ic_alert)
-            )
-        }
-
-        "Low" -> {
-            PriorityChip(
-                text = priority,
-                backgroundColor = colors.greenAccent,
-                icon = painterResource(id = R.drawable.ic_trade_down)
-            )
-        }
-    }
-}
+//@Composable
+//fun Priority(priority: String) {
+//    when (priority) {
+//        "High" -> {
+//            PriorityChip(
+//                text = priority,
+//                backgroundColor = colors.pinkAccent,
+//                icon = painterResource(id = R.drawable.ic_flag)
+//            )
+//        }
+//
+//        "Medium" -> {
+//            PriorityChip(
+//                text = priority,
+//                backgroundColor = colors.yellowAccent,
+//                icon = painterResource(id = R.drawable.ic_alert)
+//            )
+//        }
+//
+//        "Low" -> {
+//            PriorityChip(
+//                text = priority,
+//                backgroundColor = colors.greenAccent,
+//                icon = painterResource(id = R.drawable.ic_trade_down)
+//            )
+//        }
+//    }
+//}
 
 //@Preview(apiLevel = 34)
 //@Composable
