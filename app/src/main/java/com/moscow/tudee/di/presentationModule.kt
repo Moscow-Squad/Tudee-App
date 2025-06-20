ed74e32701e00b4d1e49179dd98a718f7be2e0a0
Zeinab979 <zeinabwahdan3677@gmail.com>
2025-06-20 11:36:13 +0300

    feat : add task view model in presentation module

--- app/src/main/java/com/moscow/tudee/di/presentationModule.kt
+++ app/src/main/java/com/moscow/tudee/di/presentationModule.kt
@@ -1,9 +1,11 @@
 package com.moscow.tudee.di
 
+import com.moscow.tudee.presentation.task.TaskViewModel
 import com.moscow.tudee.presentation.ui.splash.viewmodel.SplashViewModel
 import org.koin.core.module.dsl.viewModelOf
 import org.koin.dsl.module
 
 val presentationModule = module {
     viewModelOf(::SplashViewModel)
+    viewModelOf(::TaskViewModel)
 }
