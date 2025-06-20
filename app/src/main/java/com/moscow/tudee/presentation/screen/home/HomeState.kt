d3b104f2f4ca4b17626909c2ac5d3bc1f9e79e2f
Zyad <zyadhammad531@gmail.com>
2025-06-20 04:02:08 +0300

    refactor: made a task home ui model to hold the category of the task

--- app/src/main/java/com/moscow/tudee/presentation/screen/home/HomeState.kt
+++ app/src/main/java/com/moscow/tudee/presentation/screen/home/HomeState.kt
@@ -1,20 +1,22 @@
 package com.moscow.tudee.presentation.screen.home
 
 import com.moscow.tudee.domain.entity.Category
-import com.moscow.tudee.domain.entity.Task
+import com.moscow.tudee.domain.entity.Task.Priority
+import com.moscow.tudee.domain.entity.Task.Status
+import kotlinx.datetime.LocalDateTime
 
 data class HomeState(
     val isLoading: Boolean = true,
     val date: String? = null,
     val update: SliderState = SliderState.NOTHING_ON_YOUR_LIST,
-    val doneTasks: List<Task> = emptyList(),
-    val inProgressTasks: List<Task> = emptyList(),
-    val todoTasks: List<Task> = emptyList(),
-    val selectedTask: Task? = null,
+    val doneTasks: List<HomeTask> = emptyList(),
+    val inProgressTasks: List<HomeTask> = emptyList(),
+    val todoTasks: List<HomeTask> = emptyList(),
+    val selectedTask: HomeTask? = null,
     val categories: List<Category> = emptyList(),
     val showAddTaskBottomSheet: Boolean = false,
     val showEditTaskBottomSheet: Boolean = false,
-    val showTaskDetailsBottomSheet:Boolean = false
+    val showTaskDetailsBottomSheet: Boolean = false
 ) {
     enum class SliderState {
         STAY_WORKING,
@@ -22,4 +24,14 @@ data class HomeState(
         ZERO_PROGRESS,
         NOTHING_ON_YOUR_LIST
     }
+
+    data class HomeTask(
+        val id: Long? = null,
+        val title: String,
+        val description: String,
+        val priority: Priority,
+        val status: Status,
+        val date: LocalDateTime,
+        val category: Category? = null
+    )
 }
