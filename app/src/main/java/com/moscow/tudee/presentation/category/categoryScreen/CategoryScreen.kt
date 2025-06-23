73b2c98a72195f68037083fec42783edbb3d70de
Fares <dev.faresmohamed@gmail.com>
2025-06-23 12:29:35 +0300

    refactor: enhance category section screens

--- app/src/main/java/com/moscow/tudee/presentation/category/categoryScreen/CategoryScreen.kt
+++ app/src/main/java/com/moscow/tudee/presentation/category/categoryScreen/CategoryScreen.kt
@@ -1,147 +1,176 @@
 package com.moscow.tudee.presentation.category.categoryScreen
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
-import androidx.compose.material3.Button
-import androidx.compose.material3.Text
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Snackbar
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import coil.compose.rememberAsyncImagePainter
-import com.moscow.tudee.domain.entity.Task
+import com.moscow.tudee.R
 import com.moscow.tudee.presentation.ObserveAsEvent
-import com.moscow.tudee.presentation.category.categoryScreen.component.AddCategoryBottomSheet
-import com.moscow.tudee.presentation.component.bottomSheet.DeleteBottomSheet
+import com.moscow.tudee.presentation.category.CategoriesEvents
+import com.moscow.tudee.presentation.category.CategoriesScreenState
+import com.moscow.tudee.presentation.category.categoryScreen.component.CategoryBottomSheet
+import com.moscow.tudee.presentation.component.CustomFAB
 import com.moscow.tudee.presentation.designSystem.component.CategoryCard
-import com.moscow.tudee.presentation.designSystem.theme.Theme.colors
-import com.moscow.tudee.presentation.designSystem.theme.Theme.textStyle
+import com.moscow.tudee.presentation.designSystem.component.SnackBar
+import com.moscow.tudee.presentation.designSystem.component.TopBar
+import com.moscow.tudee.presentation.designSystem.theme.Theme
 import com.moscow.tudee.presentation.util.getPredefinedIconRes
 import org.koin.androidx.compose.koinViewModel
 
 @Composable
 fun CategoryScreen(
     viewModel: CategoryViewModel = koinViewModel(),
-    navigateToCategoryTasks : (Long) -> Unit
+    navigateToCategoryTasks: (Long) -> Unit
 ) {
     val uiState = viewModel.uiState.collectAsStateWithLifecycle()
     ObserveAsEvent(viewModel.uiEvent) { event ->
         when (event) {
             is CategoriesEvents.NavigateToTasks -> {
-               navigateToCategoryTasks( event.categoryId)
+                navigateToCategoryTasks(event.categoryId)
             }
         }
-
     }
     CategoryContent(
-        uiState.value,
-        viewModel
+        uiState.value, viewModel
     )
 }
 
 @Composable
 fun CategoryContent(
     uiState: CategoriesScreenState,
-    categoriesInteractionListener: CategoriesInteractionListener,
+    listener: CategoriesInteractionListener,
 ) {
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .background(colors.surface)
-            .padding(16.dp)
-    ) {
-
-        Row(
-            modifier = Modifier
-                .fillMaxWidth()
-                .height(64.dp)
-                .background(colors.surfaceHigh),
-            verticalAlignment = Alignment.CenterVertically
-        ) {
-            Text( // Should be replace to topBar?
-                text = "Categories",
-                style = textStyle.title.large,
-                color = colors.title
-            )
-        }
-
+    Scaffold(
+        topBar = {
+            CategoriesTopBar()
+        },
+        floatingActionButton = {
+            AddCategoryFAB(listener)
+        },
+    ) { padding ->
         LazyVerticalGrid(
             columns = GridCells.Fixed(3),
             contentPadding = PaddingValues(vertical = 12.dp),
+            modifier = Modifier.padding(padding),
             horizontalArrangement = Arrangement.spacedBy(8.dp),
             verticalArrangement = Arrangement.spacedBy(24.dp)
         ) {
-            items(uiState.categories) { currentCategory ->
-                if(currentCategory.isPredefined){
-                    CategoryCard(
-                        icon = painterResource(getPredefinedIconRes(currentCategory.title)),
-                        label = currentCategory.title,
-                        count = currentCategory.numberOfTasksInCategory,
-                        iconTint = Color.Unspecified,
-                        modifier = Modifier.clickable(onClick = {
-                            categoriesInteractionListener.onCategoryClick(currentCategory.id)
-                        })
-                    )
-                }else {
-                    CategoryCard(
-                        icon = rememberAsyncImagePainter(currentCategory.iconUrl),
-                        label = currentCategory.title,
-                        count = currentCategory.numberOfTasksInCategory,
-                        iconTint = Color.Unspecified,
-                        modifier = Modifier.clickable(onClick = {
-                            categoriesInteractionListener.onCategoryClick(currentCategory.id)
-                        })
-                    )
+            items(
+                items = uiState.categories, key = { it.id }) { currentCategory ->
+                val iconPainter = if (currentCategory.isPredefined) {
+                    painterResource(getPredefinedIconRes(currentCategory.title))
+                } else {
+                    rememberAsyncImagePainter(currentCategory.iconUrl)
                 }
 
+                CategoryCard(
+                    icon = iconPainter,
+                    label = currentCategory.title,
+                    count = currentCategory.numberOfTasksInCategory,
+                    iconTint = Color.Unspecified,
+                    modifier = Modifier.clickable {
+                        listener.onCategoryClick(currentCategory.id)
+                    })
             }
         }
-//        Button(onClick = { categoriesInteractionListener.onShowAddCategoryBottomSheet() }) {
-//            if (uiState.isAddCategoryBottomSheetShow)
-//                AddCategoryBottomSheet(
-//                    onNewCategory = { categoriesInteractionListener.onHideAddCategoryBottomSheet()},
-//                    onDismissRequest = { categoriesInteractionListener.onHideAddCategoryBottomSheet()},
-//                )
-//        }
     }
-}
 
-@Preview
-@Composable
-fun CategoryScreenPreview() {
-    CategoryContent(
-        uiState = CategoriesScreenState(
-
-            isAddCategoryBottomSheetShow = false
-        ),
-        categoriesInteractionListener = object : CategoriesInteractionListener {
-
-            override fun onCategoryClick(categoryID: Long) {
-                TODO("Not yet implemented")
-            }
-
-
-            override fun onShowAddCategoryBottomSheet() {}
-            override fun onHideAddCategoryBottomSheet() {
-                TODO("Not yet implemented")
-            }
-
-
+    if (uiState.isSnackBarShow) {
+        val icon = if (uiState.successMessage != null) {
+            painterResource(id = R.drawable.ic_checkmark_badge)
+        } else {
+            painterResource(id = R.drawable.ic_information_diamond)
         }
-    )
+
+        val messageId = uiState.successMessage ?: uiState.errorMessage
+        messageId?.let {
+            SnackBar(
+                icon = icon,
+                message = stringResource(id = it),
+                iconBackground = if (uiState.successMessage != null)
+                    Theme.colors.greenVariant
+                else Theme.colors.errorVariant,
+                iconTint = if (uiState.successMessage != null)
+                    Theme.colors.greenAccent
+                else Theme.colors.error
+            )
+
+
+            LaunchedEffect(Unit) {
+                kotlinx.coroutines.delay(3000)
+                listener.onHideSnackBar()
+            }
+        }
+
+
+    }
+    if (uiState.isAddCategoryBottomSheetShow)
+        CategoryBottomSheet(
+            onConfirm = { title, iconUrl ->
+                listener.onAddCategory(
+                    CategoriesScreenState.CategoryUi(
+                        title = title,
+                        iconUrl = iconUrl.toString(),
+                    )
+                )
+            },
+            onDismiss = { listener.onHideAddCategoryBottomSheet() },
+        )
 }
+    @Composable
+    private fun AddCategoryFAB(categoriesInteractionListener: CategoriesInteractionListener) {
+        CustomFAB(
+            icon = R.drawable.ic_add,
+            onClick = { categoriesInteractionListener.onShowAddCategoryBottomSheet() })
+    }
+
+    @Composable
+    private fun CategoriesTopBar() {
+        TopBar(
+            title = "Categories",
+        )
+    }
+
+
+    @Preview
+    @Composable
+    fun CategoryScreenPreview() {
+        CategoryContent(
+            uiState = CategoriesScreenState(
+
+                isAddCategoryBottomSheetShow = false
+            ), listener = object : CategoriesInteractionListener {
+
+                override fun onCategoryClick(categoryID: Long) {
+                    TODO("Not yet implemented")
+                }
+
+                override fun onHideSnackBar() {
+                    TODO("Not yet implemented")
+                }
+
+                override fun onShowAddCategoryBottomSheet() {}
+                override fun onAddCategory(category: CategoriesScreenState.CategoryUi) {
+                    TODO("Not yet implemented")
+                }
+
+                override fun onHideAddCategoryBottomSheet() {
+                    TODO("Not yet implemented")
+                }
+            })
+    }
---
+++ app/src/main/java/com/moscow/tudee/presentation/category/categoryTasksScreen/CategoryTasksScreen.kt
@@ -0,0 +1,234 @@
+package com.moscow.tudee.presentation.category.categoryTasksScreen
+
+import android.net.Uri
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.moscow.tudee.R
+import com.moscow.tudee.domain.entity.Task
+import com.moscow.tudee.presentation.ObserveAsEvent
+import com.moscow.tudee.presentation.category.CategoriesEvents
+import com.moscow.tudee.presentation.category.CategoriesScreenState
+import com.moscow.tudee.presentation.category.categoryScreen.component.CategoryBottomSheet
+import com.moscow.tudee.presentation.category.categoryScreen.component.DeleteCategoryBottomSheet
+import com.moscow.tudee.presentation.category.toCategory
+import com.moscow.tudee.presentation.component.TabItem
+import com.moscow.tudee.presentation.component.Tabs
+import com.moscow.tudee.presentation.designSystem.component.SnackBar
+import com.moscow.tudee.presentation.designSystem.component.TaskCard
+import com.moscow.tudee.presentation.designSystem.component.TopBar
+import com.moscow.tudee.presentation.designSystem.theme.Theme
+import org.koin.androidx.compose.koinViewModel
+
+@Composable
+fun CategoryTasksScreen(
+    categoryId: Long, viewModel: CategoryTasksViewModel = koinViewModel()
+) {
+    val uiState by viewModel.uiState.collectAsStateWithLifecycle()
+    ObserveAsEvent(viewModel.uiEvent) { event ->
+        when (event) {
+            is CategoriesEvents.NavigateToTasks -> {
+//                navigateBackToCategoryScreen()
+            }
+        }
+    }
+    CategoryTasksContent(
+        categoryId = 1L, uiState = uiState, listener = viewModel
+    )
+}
+
+@Composable
+fun CategoryTasksContent(
+    categoryId: Long,
+    uiState: CategoriesScreenState,
+    listener: CategoriesTasksInteractionListener
+) {
+    val tabs = listOf(
+        TabItem("To Do", uiState.tasks.size, Task.Status.TODO),
+        TabItem("In progress", uiState.tasks.size, Task.Status.IN_PROGRESS),
+        TabItem("Done", uiState.tasks.size, Task.Status.DONE)
+    )
+    Scaffold(
+        topBar = {
+            CategoriesTopBar(
+                uiState, listener, tabs, uiState.selectedStatus, categoryId
+            )
+
+        }) { padding ->
+        LazyColumn(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 16.dp)
+                .padding(top = 8.dp)
+                .padding(padding)
+        ) {
+            items(uiState.tasks) { task ->
+                TaskCard(
+                    category = uiState.category.toCategory(),
+                    title = task.title,
+                    description = task.description,
+                    date = task.date
+                ) {
+//                    PriorityChip(
+//                        text = task.priority,
+//                        backgroundColor = when (task.priority) {
+//                            "High" -> Theme.colors.pinkAccent
+//                            "Medium" -> Theme.colors.yellowAccent
+//                            else -> Theme.colors.greenAccent
+//                        },
+//                        icon = painterResource(
+//                            id = when (task.priority) {
+//                                "High" -> R.drawable.ic_flag
+//                                else -> R.drawable.ic_alert
+//                            }
+//                        )
+//                    )
+                }
+            }
+        }
+
+        if (uiState.isSnackBarShow) {
+            val icon = if (uiState.successMessage != null) {
+                painterResource(id = R.drawable.ic_checkmark_badge)
+            } else {
+                painterResource(id = R.drawable.ic_information_diamond)
+            }
+
+            val messageId = uiState.successMessage ?: uiState.errorMessage
+            messageId?.let {
+                SnackBar(
+                    icon = icon,
+                    message = stringResource(id = it),
+                    iconBackground = if (uiState.successMessage != null)
+                        Theme.colors.greenVariant
+                    else Theme.colors.errorVariant,
+                    iconTint = if (uiState.successMessage != null)
+                        Theme.colors.greenAccent
+                    else Theme.colors.error
+                )
+
+
+                LaunchedEffect(Unit) {
+                    kotlinx.coroutines.delay(3000)
+                    listener.onHideSnackBar()
+                }
+            }
+
+
+        }
+        if (uiState.isEditCategoryBottomSheetShow) {
+            CategoryBottomSheet(
+                initialTitle = uiState.category.title,
+                initialImageUri = uiState.category.iconUrl
+                    .takeIf(String::isNotBlank)
+                    ?.let(Uri::parse),
+                isEdit = true,
+                onConfirm = { title, imageUri ->
+                    listener.onUpdateCategory(
+                        uiState.category.copy(title = title, iconUrl = imageUri?.toString() ?: "")
+                    )
+                },
+                onDismiss = { listener.onHideEditCategoryBottomSheet() },
+                onShowDeleteCategory = {
+                    listener.onHideEditCategoryBottomSheet()
+                    listener.onShowDeleteCategoryBottomSheet()
+                }
+            )
+        }
+
+        if (uiState.isDeleteCategoryBottomSheetShow) {
+            DeleteCategoryBottomSheet(
+                title = uiState.category.title,
+                onDelete = {
+                    listener.onDeleteCategory(uiState.category)
+                },
+                onDismiss = {
+                    listener.onHideDeleteCategoryBottomSheet()
+                    listener.onShowEditCategoryBottomSheet()
+                }
+            )
+        }
+
+
+    }
+}
+
+@Composable
+private fun CategoriesTopBar(
+    uiState: CategoriesScreenState,
+    listener: CategoriesTasksInteractionListener,
+    tabs: List<TabItem>,
+    selectedStatus: Task.Status,
+    categoryId: Long
+) {
+    Column {
+        TopBar(
+            title = uiState.category.title,
+            startIcon = painterResource(id = R.drawable.ic_arrow_head_back),
+            endIcon = if (uiState.category.isPredefined) null else painterResource(id = R.drawable.ic_pencil_edit),
+            onEndClick = { listener.onShowEditCategoryBottomSheet() })
+        Tabs(
+            tabs = tabs, selectedStatus = selectedStatus, onTabClick = { status ->
+                listener.onTasksStatusClick(categoryId, status)
+            }, modifier = Modifier
+                .background(Theme.colors.surfaceHigh)
+                .padding(top = 8.dp)
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun CategoryTasksPreview() {
+    val sampleState = CategoriesScreenState(
+        category = CategoriesScreenState.CategoryUi(id = 1L, title = "Sample"),
+        categories = emptyList(),
+        tasks = listOf(
+            CategoriesScreenState.TaskUi(
+                title = "Review Flashcards",
+                description = "Study biology flashcards",
+                priority = Task.Priority.LOW,
+                date = "03/12/2025"
+            )
+        ),
+        isLoading = false,
+        isSnackBarShow = false,
+        errorMessage = null,
+        successMessage = null,
+    )
+    CategoryTasksContent(
+        categoryId = 1L,
+        uiState = sampleState,
+        listener = object : CategoriesTasksInteractionListener {
+            override fun onUpdateCategory(newCategory: CategoriesScreenState.CategoryUi) {
+                TODO("Not yet implemented")
+            }
+
+            override fun onDeleteCategory(category: CategoriesScreenState.CategoryUi) {
+                TODO("Not yet implemented")
+            }
+
+            override fun onTasksStatusClick(categoryID: Long, status: Task.Status) {}
+            override fun onShowEditCategoryBottomSheet() {}
+            override fun onHideEditCategoryBottomSheet() {}
+            override fun onShowDeleteCategoryBottomSheet() {}
+            override fun onHideDeleteCategoryBottomSheet() {}
+            override fun onHideSnackBar() {
+                TODO("Not yet implemented")
+            }
+
+        })
+}
