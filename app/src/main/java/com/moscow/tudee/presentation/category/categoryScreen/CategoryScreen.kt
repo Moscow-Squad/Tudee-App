e10e6cddedf4f56f1d334407fb928bdc954cf57a
Ahmad Kharfan <plamoplamo12@gmail.com>
2025-06-23 14:33:45 +0300

    refactor: make sure the icon saved to the internal storage

--- app/src/main/java/com/moscow/tudee/presentation/category/categoryScreen/CategoryScreen.kt
+++ app/src/main/java/com/moscow/tudee/presentation/category/categoryScreen/CategoryScreen.kt
@@ -8,11 +8,11 @@ import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Snackbar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -30,6 +30,7 @@ import com.moscow.tudee.presentation.designSystem.component.SnackBar
 import com.moscow.tudee.presentation.designSystem.component.TopBar
 import com.moscow.tudee.presentation.designSystem.theme.Theme
 import com.moscow.tudee.presentation.util.getPredefinedIconRes
+import com.moscow.tudee.presentation.util.saveUriToInternalStorage
 import org.koin.androidx.compose.koinViewModel
 
 @Composable
@@ -71,7 +72,8 @@ fun CategoryContent(
             verticalArrangement = Arrangement.spacedBy(24.dp)
         ) {
             items(
-                items = uiState.categories, key = { it.id }) { currentCategory ->
+                items = uiState.categories, key = { it.id }
+            ) { currentCategory ->
                 val iconPainter = if (currentCategory.isPredefined) {
                     painterResource(getPredefinedIconRes(currentCategory.title))
                 } else {
@@ -119,58 +121,68 @@ fun CategoryContent(
 
 
     }
+
+    val context = LocalContext.current
+
     if (uiState.isAddCategoryBottomSheetShow)
         CategoryBottomSheet(
-            onConfirm = { title, iconUrl ->
-                listener.onAddCategory(
-                    CategoriesScreenState.CategoryUi(
-                        title = title,
-                        iconUrl = iconUrl.toString(),
-                    )
-                )
+            onConfirm = { title, iconUri ->
+                if (iconUri != null) {
+                    val savedUri = context.saveUriToInternalStorage(iconUri)
+                    if (savedUri != null) {
+                        listener.onAddCategory(
+                            CategoriesScreenState.CategoryUi(
+                                title = title,
+                                iconUrl = savedUri.toString(),
+                            )
+                        )
+                    }
+                }
             },
             onDismiss = { listener.onHideAddCategoryBottomSheet() },
         )
+
 }
-    @Composable
-    private fun AddCategoryFAB(categoriesInteractionListener: CategoriesInteractionListener) {
-        CustomFAB(
-            icon = R.drawable.ic_add,
-            onClick = { categoriesInteractionListener.onShowAddCategoryBottomSheet() })
-    }
 
-    @Composable
-    private fun CategoriesTopBar() {
-        TopBar(
-            title = "Categories",
-        )
-    }
+@Composable
+private fun AddCategoryFAB(categoriesInteractionListener: CategoriesInteractionListener) {
+    CustomFAB(
+        icon = R.drawable.ic_add,
+        onClick = { categoriesInteractionListener.onShowAddCategoryBottomSheet() })
+}
+
+@Composable
+private fun CategoriesTopBar() {
+    TopBar(
+        title = "Categories",
+    )
+}
 
 
-    @Preview
-    @Composable
-    fun CategoryScreenPreview() {
-        CategoryContent(
-            uiState = CategoriesScreenState(
+@Preview
+@Composable
+fun CategoryScreenPreview() {
+    CategoryContent(
+        uiState = CategoriesScreenState(
 
-                isAddCategoryBottomSheetShow = false
-            ), listener = object : CategoriesInteractionListener {
+            isAddCategoryBottomSheetShow = false
+        ), listener = object : CategoriesInteractionListener {
 
-                override fun onCategoryClick(categoryID: Long) {
-                    TODO("Not yet implemented")
-                }
+            override fun onCategoryClick(categoryID: Long) {
+                TODO("Not yet implemented")
+            }
 
-                override fun onHideSnackBar() {
-                    TODO("Not yet implemented")
-                }
+            override fun onHideSnackBar() {
+                TODO("Not yet implemented")
+            }
 
-                override fun onShowAddCategoryBottomSheet() {}
-                override fun onAddCategory(category: CategoriesScreenState.CategoryUi) {
-                    TODO("Not yet implemented")
-                }
+            override fun onShowAddCategoryBottomSheet() {}
+            override fun onAddCategory(category: CategoriesScreenState.CategoryUi) {
+                TODO("Not yet implemented")
+            }
 
-                override fun onHideAddCategoryBottomSheet() {
-                    TODO("Not yet implemented")
-                }
-            })
-    }
+            override fun onHideAddCategoryBottomSheet() {
+                TODO("Not yet implemented")
+            }
+        })
+}
