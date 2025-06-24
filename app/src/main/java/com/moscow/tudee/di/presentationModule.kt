f986ba7bdec0471e829e3a8f22f080439314ffd2
Fares <dev.faresmohamed@gmail.com>
2025-06-24 22:07:52 +0300

    refactor: add string resources and prevent hard coded strings

--- app/src/main/java/com/moscow/tudee/di/presentationModule.kt
+++ app/src/main/java/com/moscow/tudee/di/presentationModule.kt
@@ -5,6 +5,7 @@ import com.moscow.tudee.presentation.category.categoryTasksScreen.CategoryTasksV
 import com.moscow.tudee.presentation.screen.home.HomeViewModel
 import com.moscow.tudee.presentation.task.TaskViewModel
 import com.moscow.tudee.presentation.ui.splash.viewmodel.SplashViewModel
+import org.koin.core.module.dsl.viewModel
 import org.koin.core.module.dsl.viewModelOf
 import org.koin.dsl.module
 
@@ -13,5 +14,8 @@ val presentationModule = module {
     viewModelOf(::TaskViewModel)
     viewModelOf(::HomeViewModel)
     viewModelOf(::CategoryViewModel)
-    viewModelOf(::CategoryTasksViewModel)
+    viewModel { (categoryId: Long) ->
+        CategoryTasksViewModel(categoryId, get(), get())
+    }
 }
+
