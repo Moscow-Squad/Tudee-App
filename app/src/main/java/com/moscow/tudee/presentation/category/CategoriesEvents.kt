0aa167193034d205f9ff23666c725c6ed9a2fba0
Fares <dev.faresmohamed@gmail.com>
2025-06-24 22:03:45 +0300

    refactor: modify navigation routes to category section

--- app/src/main/java/com/moscow/tudee/presentation/category/CategoriesEvents.kt
+++ app/src/main/java/com/moscow/tudee/presentation/category/CategoriesEvents.kt
@@ -1,5 +1,5 @@
 package com.moscow.tudee.presentation.category
 
 sealed class CategoriesEvents {
-    data class NavigateToTasks(val categoryId:Long) : CategoriesEvents()
+    data class NavigateToTasks(val categoryId: Long) : CategoriesEvents()
 }
