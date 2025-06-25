5bb287c512ef22de980731ea1797d123a4395ad2
Zyad <zyadhammad531@gmail.com>
2025-06-25 22:47:06 +0300

    feat: add domain exceptions

--- app/src/main/java/com/moscow/tudee/domain/exception/Exceptions.kt
+++ app/src/main/java/com/moscow/tudee/domain/exception/Exceptions.kt
@@ -1,8 +1,11 @@
 package com.moscow.tudee.domain.exception
 
-sealed class Exceptions (message: String): Exception(message){
-    class CategoryNotFound(categoryId: Long): Exceptions("Category id=$categoryId not found")
-    class TaskNotFound(taskId: Long): Exceptions("Task id=$taskId not found")
-    class InvalidStatus(status: String): Exceptions("Invalid status: $status")
-    class InvalidPriority(priority: String): Exceptions("Invalid priority: $priority")
+sealed class AppException: Throwable(){
+    data class DatabaseException(override val message: String) : AppException()
+    data class TaskNotFoundException(override val message: String) : AppException()
+    data class CouldNotAddTaskException(override val message: String) : AppException()
+    data class CategoryNotFoundException(override val message: String) : AppException()
+    data class CouldNotAddCategoryException(override val message: String) : AppException()
+    data class OnboardingStateException(override val message: String) : AppException()
+    data class UnknownException(override val message: String) : AppException()
 }
