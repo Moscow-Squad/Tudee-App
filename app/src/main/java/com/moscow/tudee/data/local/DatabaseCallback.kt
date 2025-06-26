820129884126f773a3fa28afbebdc6a6ea1bfce4
Ahmad Kharfan <plamoplamo12@gmail.com>
2025-06-26 17:48:22 +0300

    refactor: move static predefined categories from di to predefined categories provider then use it in data base callback

--- app/src/main/java/com/moscow/tudee/data/local/DatabaseCallback.kt
+++ app/src/main/java/com/moscow/tudee/data/local/DatabaseCallback.kt
@@ -1,23 +1,25 @@
+
 package com.moscow.tudee.data.local
 
 import androidx.room.RoomDatabase
 import androidx.sqlite.db.SupportSQLiteDatabase
 
-class DatabaseCallback(
-    private val predefinedTitles: List<String>
-) : RoomDatabase.Callback() {
+class DatabaseCallback : RoomDatabase.Callback() {
 
     override fun onCreate(db: SupportSQLiteDatabase) {
         super.onCreate(db)
 
         db.beginTransaction()
         try {
-            predefinedTitles.forEach { title ->
-                db.execSQL("""
-          INSERT INTO categories_table
-            (title, iconUri, isPredefined, countOfTasks)
-          VALUES (?, NULL, 1, 0)
-        """.trimIndent(), arrayOf(title))
+            PredefinedCategoryProvider.getTitles().forEach { title ->
+                db.execSQL(
+                    """
+                    INSERT INTO categories_table
+                      (title, iconUri, isPredefined, countOfTasks)
+                    VALUES (?, NULL, 1, 0)
+                    """.trimIndent(),
+                    arrayOf(title)
+                )
             }
 
             db.setTransactionSuccessful()
