a33c958cbd24acbb969bd05fd34d85fbb8093c8a
alaafawzy <alaaf9443@gmail.com>
2025-06-26 15:04:32 +0300

    refactor: adjust text styles and alignment in HomeTopAppBar

--- app/src/main/java/com/moscow/tudee/presentation/component/topbar/HomeTopAppBar.kt
+++ app/src/main/java/com/moscow/tudee/presentation/component/topbar/HomeTopAppBar.kt
@@ -7,15 +7,14 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.statusBarsPadding
-import androidx.compose.foundation.layout.width
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.moscow.tudee.R
@@ -31,13 +30,11 @@ fun HomeTopAppBar(
     modifier: Modifier = Modifier,
 ) {
     Row(
-        modifier =
-            modifier
-                .fillMaxWidth()
-                .background(Theme.colors.primary)
-                .statusBarsPadding()
-                .padding(vertical = 12.dp)
-                .padding(horizontal = 16.dp),
+        modifier = modifier
+            .fillMaxWidth()
+            .background(Theme.colors.primary)
+            .statusBarsPadding()
+            .padding(horizontal = 16.dp, vertical = 12.dp),
         horizontalArrangement = Arrangement.Absolute.Center,
         verticalAlignment = Alignment.CenterVertically
     ) {
@@ -49,18 +46,31 @@ fun HomeTopAppBar(
                     .padding(end = 8.dp)
 
         )
-        Column {
+        Column(
+            modifier = Modifier.padding(vertical = 4.dp)
+                .align(Alignment.Top),
+            verticalArrangement = Arrangement.Top,
+            horizontalAlignment = Alignment.Start
+        ) {
             TudeeText(
                 text = title,
                 fontFamily = CherryBomb,
                 fontSize = 18.sp,
+                lineHeight = 18.sp,
+                textAlign = TextAlign.Center,
+                fontWeight = FontWeight(400),
                 color = Theme.colors.onPrimary.copy(.87f),
-            )
+
+                )
             TudeeText(
                 text = subTitle,
                 color = Theme.colors.onPrimaryCaption.copy(.70f),
+                fontSize = 12.sp,
+                lineHeight = 16.sp,
+                textAlign = TextAlign.Center,
+                fontWeight = FontWeight(500),
                 style = Theme.textStyle.label.small,
-                )
+            )
         }
 
         Spacer(modifier = Modifier.weight(1f))
